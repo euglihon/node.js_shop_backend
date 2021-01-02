ce1200f5c927e88305a29d0e5a188e46ef44a403
euglihon <eug.lihon@hotmail.com>
2021-01-02 19:13:30 +0100

    add new controllers

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const adminController = require('../controllers/admin');
+const express = require("express");
+const adminController = require("../controllers/admin");
 const router = express.Router();
 
-
 // route ==> /admin/products ==> GET
-router.get('/products', adminController.getProducts);
-
+router.get("/products", adminController.getProducts);
 
 // route ==> /admin/add-product ==> GET
-router.get('/add-product', adminController.getAddProduct);
-
+router.get("/add-product", adminController.getAddProduct);
 
 // route ==> /admin/add-product ==> POST
-router.post('/add-product', adminController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
 
+// route ==> /admin/edit-product/:productID ==> GET
+router.get("/edit-product/:productID", adminController.getEditProduct);
+
+// route ==> /admin/edit-product ==> POST
+router.post("/edit-product", adminController.postEditProduct);
+
+// route ==> /admin/delete-product ==> POST
+router.post("/delete-product", adminController.postDeleteProduct);
 
 module.exports = router;
