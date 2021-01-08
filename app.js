cf40038103ca905fc892f1efd4064aa3aa8b8ec7
euglihon <eug.lihon@hotmail.com>
2021-01-08 18:40:34 +0100

    added order and orderItem models

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 const app = express();
 
@@ -46,17 +48,21 @@ app.use(shopRoutes);
 app.use("/", errorController.get404);
 
 // add models relation
-// one to many
 User.hasMany(Product);
 Product.belongsTo(User, { constrains: true, onDelete: "CASCADE" });
-// one to one
+
 User.hasOne(Cart);
 Cart.belongsTo(User);
-// many to many
+
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
-// automatic tables creation based on models
+User.hasMany(Order);
+Order.belongsTo(User);
+
+Order.belongsToMany(Product, { through: OrderItem });
+Product.belongsToMany(Order, { through: OrderItem });
+
 sequelize
   // .sync({ force: true })
   .sync()
