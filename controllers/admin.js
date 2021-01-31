8dd62dfd5860291ad352b46c14e40a0dba153238
euglihon <eug.lihon@hotmail.com>
2021-01-31 19:13:16 +0100

    add validation POST add-product and edit-product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,7 @@
 const Product = require("../models/product");
 
+const { validationResult } = require("express-validator/check");
+
 exports.getProducts = (req, res) => {
   Product.find({ userID: req.user._id }) // mongoose method
     .then((products) => {
@@ -19,12 +21,35 @@ exports.getAddProduct = (req, res) => {
     activePath: "/admin/add-product",
     editing: false,
     isAuthenticated: req.session.isLoggedIn,
+    hasError: false,
+    errorMessage: null,
+    validationErrors: [],
   });
 };
 
 exports.postAddProduct = (req, res) => {
   // req.body ==> html input form data (edit-product.pug) create product
   const { title, description, price, imageURL } = req.body;
+
+  // add validation
+  const validationError = validationResult(req);
+  if (!validationError.isEmpty()) {
+    return res.status(422).render("admin/edit-product.pug", {
+      docTitle: "Add Product",
+      activePath: "/admin/add-product",
+      editing: false,
+      isAuthenticated: req.session.isLoggedIn,
+      product: {
+        title: title,
+        description: description,
+        price: price,
+        imageURL: imageURL,
+      },
+      hasError: true,
+      errorMessage: validationError.array()[0].msg,
+      validationErrors: validationError.array(),
+    });
+  }
 
   const product = new Product({
     title: title,
@@ -58,11 +83,14 @@ exports.getEditProduct = (req, res) => {
           res.redirect("/");
         } else {
           res.render("admin/edit-product.pug", {
-            docTitle: `Edit ${product.title}`,
+            docTitle: `Edit product`,
             activePath: "/admin/edit-product",
             editing: Boolean(editParam), // string true to boolean true, js:)
             product: product,
             isAuthenticated: req.session.isLoggedIn,
+            hasError: false,
+            errorMessage: null,
+            validationErrors: [],
           });
         }
       })
@@ -76,6 +104,26 @@ exports.postEditProduct = (req, res) => {
 
   // req.body ==> html input form data (edit-product.pug) update product
   const { title, description, price, imageURL } = req.body;
+
+  // add validation
+  const validationError = validationResult(req);
+  if (!validationError.isEmpty()) {
+    return res.status(422).render("admin/edit-product.pug", {
+      docTitle: "Edit Product",
+      activePath: "/admin/edit-product",
+      editing: false,
+      isAuthenticated: req.session.isLoggedIn,
+      product: {
+        title: title,
+        description: description,
+        price: price,
+        imageURL: imageURL,
+      },
+      hasError: true,
+      errorMessage: validationError.array()[0].msg,
+      validationErrors: validationError.array(),
+    });
+  }
 
   // update product
   Product.findById(productID) // mongoose method
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { check, body } = require("express-validator/check");
+
 const adminController = require("../controllers/admin");
+
 const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
@@ -9,14 +12,68 @@ router.get("/products", isAuth, adminController.getProducts);
 // route ==> /admin/add-product ==> GET  and add isAuthn middleware
 router.get("/add-product", isAuth, adminController.getAddProduct);
 
-// route ==> /admin/add-product ==> POST  and add isAuthn middleware
-router.post("/add-product", isAuth, adminController.postAddProduct);
+// route ==> /admin/add-product ==> POST
+router.post(
+  "/add-product",
+  // add check validation middleware
+  [
+    body("title")
+      .isString()
+      .isLength({ min: 3, max: 140 })
+      .trim()
+      .withMessage("please enter the correct title"),
 
-// route ==> /admin/edit-product/:productID ==> GET  and add isAuthn middleware
-router.get("/edit-product/:productID", isAuth, adminController.getEditProduct);
+    body("description")
+      .isString()
+      .isLength({ min: 10, max: 300 })
+      .trim()
+      .withMessage("please enter the correct description"),
 
-// route ==> /admin/edit-product ==> POST  and add isAuthn middleware
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+    body("price")
+      .isFloat()
+      .trim()
+      .withMessage("please enter the correct price"),
+
+    body("imageURL").isURL().withMessage("please enter the correct url"),
+  ],
+  // add isAuthn middleware
+  isAuth,
+  adminController.postAddProduct
+);
+
+// route ==> /admin/edit-product/:productID ==> GET
+router.get(
+  "/edit-product/:productID", // add check validation middleware
+  isAuth, // add isAuthn middleware
+  adminController.getEditProduct
+);
+
+// route ==> /admin/edit-product ==> POST
+router.post(
+  "/edit-product",
+  [
+    body("title")
+      .isString()
+      .isLength({ min: 3, max: 140 })
+      .trim()
+      .withMessage("please enter the correct title"),
+
+    body("description")
+      .isString()
+      .isLength({ min: 10, max: 300 })
+      .trim()
+      .withMessage("please enter the correct description"),
+
+    body("price")
+      .isFloat()
+      .trim()
+      .withMessage("please enter the correct price"),
+
+    body("imageURL").isURL().withMessage("please enter the correct url"),
+  ],
+  isAuth, // add isAuthn middleware
+  adminController.postEditProduct
+);
 
 // route ==> /admin/delete-product ==> POST  and add isAuthn middleware
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
